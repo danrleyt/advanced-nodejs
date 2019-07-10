0bc13da3e5141c729dade2d1576f4dd523ded920
danrley.teixeira <danrley.teixeira@dataprev.gov.br>
2019-07-10 15:00:33 -0300

    Just code to test pm2

---
+++ clustering/index.js
@@ -0,0 +1,16 @@
+//Child, act like a server and do nothing
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hi There!');
+  });
+});
+
+app.get('/fast', (req, res) => {
+  res.send('This was fast');
+});
+
+app.listen(3000);
